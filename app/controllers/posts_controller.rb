b74bc5824a7591f7a2c53bc82728c51241a9fa00
lizlove <elizabethlovero@gmail.com>
2015-08-06 14:08:20 -0400

    models with form

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+  class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.create(post_params) 
+    redirect_to posts_path(@post)
+  end
+
+  # def update
+  #   respond_to do |format|
+  #     if @post.update(post_params)
+  #       format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+  #     else
+  #       format.html { render :edit }
+  #     end
+  #   end
+  # end
+
+  # def destroy
+  #   @frog.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+  #   end
+  # end
+
+private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:resentful_at, :cause, :affects_my, :my_part, :selfish, :dishonest, :afraid, :owe_an_apology, :kept_to_myself, :kind_and_loving, :done_better, :thinking_of_myself, :thinking_of_others, :defects, :gratitude)
+    end
+
+end
---
+++ db/migrate/20150728064518_create_posts.rb
@@ -0,0 +1,22 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :resentful_at
+      t.text :cause
+      t.text :affects_my
+      t.text :my_part
+      t.text :selfish
+      t.text :dishonest
+      t.text :afraid
+      t.text :owe_an_apology
+      t.text :kept_to_myself
+      t.text :kind_and_loving
+      t.text :done_better
+      t.text :thinking_of_myself
+      t.text :thinking_of_others
+      t.text :defects
+      t.text :gratitude
+      t.timestamps null: false
+    end
+  end
+end
