7f9cf240382afaa64c1d7b157127cbe19b5dad08
lizlove <elizabethlovero@gmail.com>
2015-08-28 21:56:45 -0400

    update email view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create :sponsor_add_notification
+
+  private
+
+  def sponsor_add_notification
+    NightlyMailer.sponsor_add(self).deliver
+  end 
+
 end
