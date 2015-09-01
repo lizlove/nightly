d7dc11bfb21528ad6a9c2de2ade3b81dd9785311
lizlove <elizabethlovero@gmail.com>
2015-09-01 14:12:54 -0400

    updated user and sponsor emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
   has_many :posts
-  devise :database_authenticatable, :confirmable, :recoverable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :registerable, :rememberable, :validatable
+  after_create :sponsor_add_notification
 
   private
+
+  def user_register_notification
+    NightlyMailer.user_register(self).deliver
+    @user = user
+    @sponsor = @user.sponsor_email
+    mail to: @user.email, subject: "You have registered for Nightly"
+  end
 
   def sponsor_add_notification
     NightlyMailer.sponsor_add(self).deliver
