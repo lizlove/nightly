9062552a7d92c05adb1b247164d1bd45d9990b90
lizlove <elizabethlovero@gmail.com>
2020-03-24 10:27:35 -0400

    Update tracking.

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,4 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 #configure ActionMailer for MailGun
-ActionMailer::Base.smtp_settings = {
-  :port           => 587,
-  :address        => "smtp.mailgun.org",
-  :domain         => ENV['domain'],
-  :user_name      => ENV['username'],
-  :password       => ENV['password'],
-  :authentication => :plain,
-}
 
